be18a21ef900c284d168ed7282dfed103d94fe8a
ikawamuk <ikawamuk@student.42.fr>
2026-03-16 21:36:34 +0900

    ok

---
+++ My_CPP07/ex02/Array.hpp
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/16 20:00:21 by ikawamuk          #+#    #+#             */
+/*   Updated: 2026/03/16 21:22:02 by ikawamuk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP_
+# define ARRAY_HPP_
+
+# include <cstddef>
+# include <exception>
+# include <stdexcept>
+
+template <typename T>
+class Array
+{
+	public:
+					Array(void);
+					Array(unsigned int n);
+					Array(const Array& other);
+		Array&		operator=(const Array& rhs);
+					~Array();
+		std::size_t	size(void) const;
+		T&			operator[](std::size_t index);
+		const T&	operator[](std::size_t index) const;
+	private:
+		T			*elements_;
+		std::size_t	size_;
+};
+
+template <typename T>
+Array<T>::Array(void)
+: elements_(NULL), size_(0)
+{
+}
+
+template <typename T>
+Array<T>::Array(unsigned int n)
+: elements_(NULL), size_(n)
+{
+	if (n == 0)
+		return;
+	elements_ = new T[n]();
+}
+
+template <typename T>
+Array<T>::Array(const Array& other)
+: elements_(NULL), size_(other.size_)
+{
+	if (size_ == 0)
+		return;
+	elements_ = new T[size_]();
+	for (std::size_t i = 0; i < size_; ++i)
+		elements_[i] = other.elements_[i];
+}
+
+template <typename T>
+Array<T>& Array<T>::operator=(const Array& rhs)
+{
+	if (this == &rhs)
+		return (*this);
+	delete [] elements_;
+	elements_ = NULL;
+	size_ = rhs.size_;
+	if (size_ == 0)
+		return (*this);
+	elements_ = new T[size_]();
+	for (std::size_t i = 0; i < size_; ++i)
+		elements_[i] = rhs.elements_[i];
+	return (*this);
+}
+
+template <typename T>
+Array<T>::~Array()
+{
+	delete[] elements_;
+}
+
+template <typename T>
+std::size_t Array<T>::size(void) const
+{
+	return (size_);
+}
+
+template <typename T>
+T& Array<T>::operator[](std::size_t index)
+{
+	if (index >= size_)
+		throw std::out_of_range("Array index out of bounds");
+	return (elements_[index]);
+}
+
+template <typename T>
+const T& Array<T>::operator[](std::size_t index) const
+{
+	if (index >= size_)
+		throw std::out_of_range("Array index out of bounds");
+	return (elements_[index]);
+}
+
+#endif
---
+++ My_CPP07/ex02/main.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <cstdlib>
+#include "Array.hpp"
+
+#define MAX_VAL 750
+
+int		given_test(int, char**);
+void	copy_constructor_assignment_test(void);
+void	out_of_range_test(void);
+void	cleanup_test(void);
+
+int	main(int ac, char* av[])
+{
+	given_test(ac, av);
+	return (0);
+}
+
+int given_test(int, char**)
+{
+	copy_constructor_assignment_test();
+	out_of_range_test();
+	cleanup_test();
+	return 0;
+}
+
+void	copy_constructor_assignment_test(void)
+{
+	Array<int> numbers(MAX_VAL);
+	int* mirror = new int[MAX_VAL];
+	srand(time(NULL));
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		const int value = rand();
+		numbers[i] = value;
+		mirror[i] = value;
+	}
+	Array<int> tmp = numbers;
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		if (tmp[i] != numbers[i])
+		{
+			std::cerr << "copy mismatch: tmp" << std::endl;
+			delete [] mirror;
+			return ;
+		}
+	}
+	Array<int> test(tmp);
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		if (test[i] != numbers[i])
+		{
+			std::cerr << "copy mismatch: test" << std::endl;
+			delete [] mirror;
+			return ;
+		}
+	}
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		if (mirror[i] != numbers[i])
+		{
+			std::cerr << "didn't save the same value!!" << std::endl;
+			return ;
+		}
+	}
+	std::cout << "copy_constructor_assignment_test: SUCCESS" << std::endl;
+}
+
+void	out_of_range_test(void)
+{
+	Array<int> numbers(MAX_VAL);
+	int* mirror = new int[MAX_VAL];
+	srand(time(NULL));
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		const int value = rand();
+		numbers[i] = value;
+		mirror[i] = value;
+	}
+	try
+	{
+		numbers[-2] = 0;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Error message is" << e.what() << '\n';
+	}
+	try
+	{
+		numbers[MAX_VAL] = 0;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Error message is" << e.what() << '\n';
+	}
+	std::cout << "out_of_range_test: SUCCESS(if errore messages are shown currectly)" << std::endl;
+}
+
+void	cleanup_test(void)
+{
+	Array<int> numbers(MAX_VAL);
+	int* mirror = new int[MAX_VAL];
+	srand(time(NULL));
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		const int value = rand();
+		numbers[i] = value;
+		mirror[i] = value;
+	}
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		numbers[i] = rand();
+	}
+	delete [] mirror;
+	std::cout << "cleanup_test: SUCCESS" << std::endl;
+}
